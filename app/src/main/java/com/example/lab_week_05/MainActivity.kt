20725693e5fdbc474759ba1e9b06cdde6bcf6ef2
jiversond <141613198+jiversond@users.noreply.github.com>
2025-09-27 23:07:54 +0700

    Commit No. 1 - Retrofit Setup and Fetch API JSON Response

---
+++ app/src/main/java/com/example/lab_week_05/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.lab_week_05
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.lab_week_05.api.CatApiService
+import retrofit2.*
+import retrofit2.converter.scalars.ScalarsConverterFactory
+
+class MainActivity : AppCompatActivity() {
+
+    // Retrofit instance
+    private val retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl("https://api.thecatapi.com/v1/")
+            .addConverterFactory(ScalarsConverterFactory.create())
+            .build()
+    }
+
+    // CatApiService instance
+    private val catApiService by lazy {
+        retrofit.create(CatApiService::class.java)
+    }
+
+    // TextView untuk menampilkan response
+    private val apiResponseView: TextView by lazy {
+        findViewById(R.id.api_response)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        getCatImageResponse()
+    }
+
+    private fun getCatImageResponse() {
+        val call = catApiService.searchImages(1, "full")
+        call.enqueue(object : Callback<String> {
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                Log.e(MAIN_ACTIVITY, "Failed to get response", t)
+            }
+
+            override fun onResponse(call: Call<String>, response: Response<String>) {
+                if (response.isSuccessful) {
+                    apiResponseView.text = response.body()
+                } else {
+                    Log.e(
+                        MAIN_ACTIVITY,
+                        "Failed to get response\n${response.errorBody()?.string().orEmpty()}"
+                    )
+                }
+            }
+        })
+    }
+
+    companion object {
+        const val MAIN_ACTIVITY = "MAIN_ACTIVITY"
+    }
+}
